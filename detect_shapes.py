fd9f876591fac2fc9ca1cffeedfc5ed6dc675c21
JoseFreixo <josefreixo15@gmail.com>
2019-10-22 16:48:54 +0100

    Code cleaned

--- detect_shapes.py
+++ detect_shapes.py
@@ -3,52 +3,12 @@ import argparse
 import imutils
 import cv2
 import numpy as np
-
-
-class ShapeDetector:
-	def __init__(self):
-		pass
- 
-	def detect(self, c):
-		# initialize the shape name and approximate the contour
-		shape = "unidentified"
-		peri = cv2.arcLength(c, True)
-		# O valor no peri diferencia a shape detetada
-		approx = cv2.approxPolyDP(c, 0.005 * peri, True) 
-
-        # if the shape is a triangle, it will have 3 vertices
-		if len(approx) == 3:
-			shape = "triangle"
- 
-		# if the shape has 4 vertices, it is either a square or
-		# a rectangle
-		if len(approx) == 4:
-			# compute the bounding box of the contour and use the
-			# bounding box to compute the aspect ratio
-			(x, y, w, h) = cv2.boundingRect(approx)
-			ar = w / float(h)
- 
-			# a square will have an aspect ratio that is approximately
-			# equal to one, otherwise, the shape is a rectangle
-			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
- 
-		# if the shape is a pentagon, it will have 5 vertices
-		elif len(approx) == 5:
-			shape = "pentagon"
- 
-		# otherwise, we assume the shape is a circle
-		else:
-			shape = "circle"
- 
-		# return the name of the shape
-		return shape
  
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
 args = vars(ap.parse_args())
-
 
 # load the image and resize it to a smaller factor so that
 # the shapes can be approximated better
@@ -80,18 +40,9 @@ thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
-sd = ShapeDetector()
-
-# print(cnts.size)
 
 # loop over the contours
 for c in cnts:
-	# compute the center of the contour, then detect the name of the
-	# shape using only the contour
-	M = cv2.moments(c)
-	cX = int((M["m10"] / M["m00"]) * ratio)
-	cY = int((M["m01"] / M["m00"]) * ratio)
-	shape = sd.detect(c)
  
 	# multiply the contour (x, y)-coordinates by the resize ratio,
 	# then draw the contours and the name of the shape on the image
@@ -99,8 +50,12 @@ for c in cnts:
 	c *= ratio
 	c = c.astype("int")
 	cv2.drawContours(image, [c], -1, (0, 255, 0), 1)
-	# cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-	# 	0.5, (255, 255, 255), 2)
+
+	# Detect the bounding rectangle of the contour
+	# x and y are the coordinates of the top left vertice
+	# w and h are the width and height, respectively
+	(x, y, w, h) = cv2.boundingRect(c)
+	print((x, y, w, h))
  
 	# show the output image
 	cv2.imshow("Image", image)
