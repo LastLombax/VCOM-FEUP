f54f399ccfd666191b6c7b50d25d0b12ab9f9228
LastLombax <up201503447@fe.up.pt>
2019-10-25 17:36:25 +0100

    added stretching to connect files but there are some errors

--- detect_barcode.py
+++ detect_barcode.py
@@ -73,12 +73,71 @@ def findCountors(closed):
 	box = np.int0(box)
 	return box
 
-showProgressInWindow(image)
-(image, gray) = loadImage()
-(closed, concatImages) = MainAlgorithm(gray)
-box = findCountors(concatImages)
+def stretch(img, box):
+	# reshape borders
+	pts = box.reshape(4,2)
+	rect = np.zeros((4, 2), dtype="float32")
+	
 
-# draw a bounding box arounded the detected barcode and display the image
-cv2.drawContours(image, [box], -1, (0, 255, 0), 1)
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+	# the top-left point has the smallest sum whereas the
+    # bottom-right has the largest sum
+	s = pts.sum(axis=1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	
+	# compute the difference between the points -- the top-right
+    # will have the minimum difference and the bottom-left will
+    # have the maximum difference
+	diff = np.diff(pts, axis=1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+
+    # multiply the rectangle by the original ratio
+	rect *= 1
+
+	 # now that we have our rectangle of points, let's compute
+    # the width of our new image
+	(tl, tr, br, bl) = rect
+	print(rect)
+	width_a = width_b = np.sqrt(((br[0] - bl[0]) * 2) + ((br[1] - bl[1]) * 2))
+
+	# ...and now for the height of our new image
+	height_a = height_b = np.sqrt(((tr[0] - br[0]) * 2) + ((tr[1] - br[1]) * 2))
+
+	# take the maximum of the width and height values to reach
+    # our final dimensions
+
+	max_width = max(int(width_a), int(width_b))
+	max_height = max(int(height_a), int(height_b))
+
+	# construct our destination points which will be used to
+    # map the screen to a top-down, "birds eye" view
+	dst = np.array([[0, 0], [max_width - 1, 0], [max_width - 1, max_height - 1], [0, max_height - 1]], dtype="float32")
+
+	 # calculate the perspective transform matrix and warp
+    # the perspective to grab the screen
+	M = cv2.getPerspectiveTransform(rect, dst)
+	return cv2.warpPerspective(img, M, (max_width, max_height)) 
+
+
+def Main():
+	(image, gray) = loadImage()
+	(closed, concatImages) = MainAlgorithm(gray)
+	showProgressInWindow(concatImages)
+	box = findCountors(closed)
+
+	# draw a bounding box arounded the detected barcode and display the image
+	cv2.drawContours(image, [box], -1, (0, 255, 0), 1)
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
+
+	warp = stretch(image, box)
+	cv2.imshow("Warp", warp)
+	cv2.waitKey(0)
+
+	
+	
+
+
+
+Main()
