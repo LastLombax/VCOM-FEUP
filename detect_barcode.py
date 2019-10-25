cd0a42e81e70d6a28e28a622cbf75f8486e5fe3b
LastLombax <up201503447@fe.up.pt>
2019-10-25 15:29:19 +0100

    organized to functions

--- detect_barcode.py
+++ detect_barcode.py
@@ -7,70 +7,78 @@ import argparse
 import imutils
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True,
-	help = "path to the image file")
-args = vars(ap.parse_args())
+# Parses argument and returns image and image in grayscale
+def loadImage():	
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required = True,
+		help = "path to the image file")
+	args = vars(ap.parse_args())
 
-# load the image and convert it to grayscale
-image = cv2.imread(args["image"])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# compute the Scharr gradient magnitude representation of the images
-# in both the x and y direction using OpenCV 2.4
-ddepth = cv2.CV_32F
-gradX = cv2.Sobel(gray, ddepth=ddepth, dx=1, dy=0, ksize=-1)
-gradY = cv2.Sobel(gray, ddepth=ddepth, dx=0, dy=1, ksize=-1)
-
-# subtract the y-gradient from the x-gradient
-gradient = cv2.subtract(gradX, gradY)
-gradient = cv2.convertScaleAbs(gradient)
-
-# blur and threshold the image
-blurred = cv2.blur(gradient, (9,9)) 	
-(_, thresh) = cv2.threshold(blurred, 223, 255, cv2.THRESH_BINARY)
-
-# construct a closing kernel and apply it to the thresholded image
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 7))  #TODO: Change this tuple. Original was (21,7)
-aux = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
-concat1 = np.hstack((blurred, aux))
-
-# perform a series of erosions and dilations
-closed = cv2.erode(aux, None, iterations = 4)
-closed = cv2.dilate(closed, None, iterations = 4)
-
-concat2 = np.hstack((concat1, closed))
+	# load the image and convert it to grayscale
+	image = cv2.imread(args["image"])
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	return image, gray
 
 
-# Shows all in one window
-screen_res = 1440, 1080
-scale_width = screen_res[0] / concat2.shape[1]
-scale_height = screen_res[1] / concat2.shape[0]
-scale = min(scale_width, scale_height)
-window_width = int(concat2.shape[1] * scale)
-window_height = int(concat2.shape[0] * scale)
-cv2.namedWindow('Resized Window', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Resized Window', window_width, window_height)
+def MainAlgorithm(gray):
+	# compute the Scharr gradient magnitude representation of the images
+	# in both the x and y direction using OpenCV 2.4
+	ddepth = cv2.CV_32F
+	gradX = cv2.Sobel(gray, ddepth=ddepth, dx=1, dy=0, ksize=-1)
+	gradY = cv2.Sobel(gray, ddepth=ddepth, dx=0, dy=1, ksize=-1)
 
-cv2.imshow('Resized Window', concat2)
+	# subtract the y-gradient from the x-gradient
+	gradient = cv2.subtract(gradX, gradY)
+	gradient = cv2.convertScaleAbs(gradient)
+
+	# blur and threshold the image
+	blurred = cv2.blur(gradient, (9,9)) 	
+	(_, thresh) = cv2.threshold(blurred, 223, 255, cv2.THRESH_BINARY)
+
+	# construct a closing kernel and apply it to the thresholded image
+	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 7)) 
+	aux = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+	concat1 = np.hstack((blurred, aux))
+
+	# perform a series of erosions and dilations
+	closed = cv2.erode(aux, None, iterations = 4)
+	closed = cv2.dilate(closed, None, iterations = 4)
+
+	concat2 = np.hstack((concat1, closed))
+	return closed, concat2
+
+# shows concatenated images in a window
+def showProgressInWindow(image):
+	screen_res = 1440, 1080
+	scale_width = screen_res[0] / image.shape[1]
+	scale_height = screen_res[1] / image.shape[0]
+	scale = min(scale_width, scale_height)
+	window_width = int(image.shape[1] * scale)
+	window_height = int(image.shape[0] * scale)
+	cv2.namedWindow('Resized Window', cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('Resized Window', window_width, window_height)
+	cv2.imshow('Resized Window', image)
 
 # find the contours in the thresholded image, then sort the contours
 # by their area, keeping only the largest one
-cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+def findCountors(closed):
+	cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+	# compute the rotated bounding box of the largest contour
+	rect = cv2.minAreaRect(c)
+	box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
+	box = np.int0(box)
+	return box
 
-# compute the rotated bounding box of the largest contour
-rect = cv2.minAreaRect(c)
-box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
-box = np.int0(box)
+showProgressInWindow(image)
+(image, gray) = loadImage()
+(closed, concatImages) = MainAlgorithm(gray)
+box = findCountors(concatImages)
 
-# draw a bounding box arounded the detected barcode and display the
-# image
-cv2.drawContours(image, [box], -1, (0, 255, 0), 1
-)
+# draw a bounding box arounded the detected barcode and display the image
+cv2.drawContours(image, [box], -1, (0, 255, 0), 1)
 cv2.imshow("Image", image)
 cv2.waitKey(0)
