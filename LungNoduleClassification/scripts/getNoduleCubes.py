a18a2ae65c16d7f58625f50184223aa452e4f2df
JoseFreixo <josefreixo15@gmail.com>
2019-12-26 15:34:51 +0000

    Updated getNoduleCubes

--- LungNoduleClassification/scripts/getNoduleCubes.py
+++ LungNoduleClassification/scripts/getNoduleCubes.py
@@ -7,7 +7,7 @@ from readNoduleList import nodEqDiam
 dispFlag = False
 
 # Read nodules csv
-csvlines = readCsv('trainNodules_gt.csv')
+csvlines = readCsv('../trainset_csv/trainNodules_gt.csv')
 header = csvlines[0]
 nodules = csvlines[1:]
 
@@ -25,7 +25,7 @@ for n in nodules:
                         
                 # Read scan
                 if lnd!=lndloaded:
-                        [scan,spacing,origin,transfmat] =  readMhd('data/LNDb-{:04}.mhd'.format(lnd))                
+                        [scan,spacing,origin,transfmat] =  readMhd('../Dataset/LNDb-{:04}.mhd'.format(lnd))
                         transfmat_toimg,transfmat_toworld = getImgWorldTransfMats(spacing,transfmat)
                         lndloaded = lnd
                 
@@ -34,27 +34,32 @@ for n in nodules:
                 
                 for rad,radfinding in zip(rads,radfindings):
                         # Read segmentation mask
-                        [mask,_,_,_] =  readMhd('masks/LNDb-{:04}_rad{}.mhd'.format(lnd,rad))
+                        # [mask,_,_,_] =  readMhd('masks/LNDb-{:04}_rad{}.mhd'.format(lnd,rad))
                         
                         # Extract cube around nodule
                         scan_cube = extractCube(scan,spacing,ctr)
-                        masknod = copy.copy(mask)
-                        masknod[masknod!=radfinding] = 0
-                        masknod[masknod>0] = 1
-                        mask_cube = extractCube(masknod,spacing,ctr)
+                        # masknod = copy.copy(mask)
+                        # masknod[masknod!=radfinding] = 0
+                        # masknod[masknod>0] = 1
+                        # mask_cube = extractCube(masknod,spacing,ctr)
                         
                         # Display mid slices from resampled scan/mask
                         if dispFlag:
                                 fig, axs = plt.subplots(2,3)
                                 axs[0,0].imshow(scan_cube[int(scan_cube.shape[0]/2),:,:])
-                                axs[1,0].imshow(mask_cube[int(mask_cube.shape[0]/2),:,:])
+                                # axs[1,0].imshow(mask_cube[int(mask_cube.shape[0]/2),:,:])
                                 axs[0,1].imshow(scan_cube[:,int(scan_cube.shape[1]/2),:])
-                                axs[1,1].imshow(mask_cube[:,int(mask_cube.shape[1]/2),:])
+                                # axs[1,1].imshow(mask_cube[:,int(mask_cube.shape[1]/2),:])
                                 axs[0,2].imshow(scan_cube[:,:,int(scan_cube.shape[2]/2)])
-                                axs[1,2].imshow(mask_cube[:,:,int(mask_cube.shape[2]/2)])    
+                                # axs[1,2].imshow(mask_cube[:,:,int(mask_cube.shape[2]/2)])
                                 plt.show()
                         
+                        # Save scan cubes
+                        axs[0,0].imsave('../Dataset/images/LNDb-{:04d}_finding{}_rad{}_x.png'.format(lnd, finding, rad), scan_cube[int(scan_cube.shape[0]/2),:,:])
+                        axs[0,1].imsave('../Dataset/images/LNDb-{:04d}_finding{}_rad{}_y.png'.format(lnd, finding, rad), scan_cube[int(scan_cube.shape[1]/2),:,:])
+                        axs[0,2].imsave('../Dataset/images/LNDb-{:04d}_finding{}_rad{}_z.png'.format(lnd, finding, rad), scan_cube[int(scan_cube.shape[2]/2),:,:])
+
                         # Save mask cubes
-                        np.save('mask_cubes/LNDb-{:04d}_finding{}_rad{}.npy'.format(lnd,finding,rad),mask_cube)                
+                        # np.save('mask_cubes/LNDb-{:04d}_finding{}_rad{}.npy'.format(lnd,finding,rad),mask_cube)                
                 
                 
