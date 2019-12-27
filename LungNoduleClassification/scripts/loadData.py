5c517b2e136b1c7aeb6dd4ca4a6fba4268fe8e6b
bernardoleite <up201404464@gcloud.fe.up.pt>
2019-12-27 12:33:07 +0000

    update

--- LungNoduleClassification/scripts/loadData.py
+++ LungNoduleClassification/scripts/loadData.py
@@ -4,40 +4,25 @@ import os
 import cv2
 from tqdm import tqdm
 
-DATADIR = "D:\VCOM\VCOM-FEUP\LungNoduleClassification\Dataset\images"
+DATADIR = "../Dataset/images"
 
-CATEGORIES = ["GGO", "PS", "S"]
+CATEGORIES = ["GGO", "PartSolid"]
 
-for category in CATEGORIES:  # do dogs and cats
-    path = os.path.join(DATADIR,category)  # create path to dogs and cats
-    for img in os.listdir(path):  # iterate over each image per dogs and cats
-        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-        plt.imshow(img_array, cmap='gray')  # graph it
-        plt.show()  # display!
-
-        break  # we just want one for now so break
-    break  #...and one more!
-
-IMG_SIZE = 50
-
-new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-plt.imshow(new_array, cmap='gray')
-plt.show()
-
+IMG_SIZE = 80
 
 training_data = []
 
 def create_training_data():
-    for category in CATEGORIES:  # do dogs and cats
+    for category in CATEGORIES:  # do ggo, ps and s
 
-        path = os.path.join(DATADIR,category)  # create path to dogs and cats
-        class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
+        path = os.path.join(DATADIR,category)  # create path to ggo, ps and s
+        class_num = CATEGORIES.index(category)  # get the classification  (0, 1 or 2). 
 
-        for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
+        for img in tqdm(os.listdir(path)):  # iterate over each image per ggo, ps and s
             try:
                 img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
-                training_data.append([new_array, class_num])  # add this to our training_data
+                #new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                training_data.append([img_array, class_num])  # add this to our training_data
             except Exception as e:  # in the interest in keeping the output clean...
                 pass
             #except OSError as e:
