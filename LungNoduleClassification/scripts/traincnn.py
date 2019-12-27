5478336c4c8a2cba2a531ded0da2148979e5e52e
bernardoleite <up201404464@gcloud.fe.up.pt>
2019-12-27 14:59:57 +0000

    update

--- LungNoduleClassification/scripts/traincnn.py
+++ LungNoduleClassification/scripts/traincnn.py
@@ -17,11 +17,11 @@ X = X/255.0
 
 model = Sequential()
 
-model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (3, 3)))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -36,4 +36,5 @@ model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)
+y = np.array(y)
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.3)
