722e8908a4023b92bbaaaa1ca73dbdf1f1d63b4f
JoseFreixo <josefreixo15@gmail.com>
2020-01-04 22:07:11 +0000

    Revert traincnn

--- LungNoduleClassification/scripts/traincnn.py
+++ LungNoduleClassification/scripts/traincnn.py
@@ -90,7 +90,7 @@ def performance_stats(model):
     X_test = np.array(X_test)
 
     # train the model 
-    model.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.15)
+    model.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.15, callbacks=[tensorboard])
 
     # test the model
     score = model.evaluate(X_test, y_test)
@@ -102,15 +102,10 @@ def performance_stats(model):
     # needed conversion in order to work bellow
     X_test = np.array(X_test).astype(np.float32)
 
-    # # classification report
-    # y_pred = model.predict(X_test, batch_size=64, verbose=1)
-    # y_pred_bool = np.argmax(y_pred, axis=1)
-    # print(classification_report(y_test, y_pred_bool))
-
-    # test the model
-    score = model.evaluate(X_test, y_test, callbacks=[tensorboard])
-    print("\nTest accuracy: %0.04f" % score[1])
-
+    # classification report
+    y_pred = model.predict(X_test, batch_size=64, verbose=1)
+    y_pred_bool = np.argmax(y_pred, axis=1)
+    print(classification_report(y_test, y_pred_bool))
 
 #build model
 model_1 = create_model(128, 2, 1)
